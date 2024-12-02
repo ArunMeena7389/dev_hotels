ba535def49ba8e533eeb96f7bde9f0c78fe3082a
Arun <arunmeena.738950@gmail.com>
2024-12-02 22:13:29 +0530

    for mongo db

--- db.js
+++ db.js
@@ -5,42 +5,28 @@ require('dotenv').config();
 // Mongodb URL define
 
 // const mongoURL = process.env.localdb_url //database local
-const mongoURL = "mongodb+srv://arun7389:arun7389@cluster0.g44ty.mongodb.net/"; // databse monog
+const mongoURL = process.env.mongodb_url; // databse monog
 
 
-mongoose.connect(mongoURL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-
-// module.exports = async () => {
-//     console.log(mongoURL,'--------------mongoURL');
-
-//     try {
-//         await mongoose.connect(mongoURL, {
-//             useNewUrlParser:true,
-//             useUnifiedTopology:true
-//         });
-//         console.log("CONNECTED TO DATABASE SUCCESSFULLY");
-//     } catch (error) {
-//         console.error('COULD NOT CONNECT TO DATABASE:', error.message);
-//     }
-// };
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
-db.on('connected',()=>{
+db.on('connected', () => {
     console.log("mongo db connected------yes");
-    
+
 })
 
-db.on('error',(error)=>{
-    console.log("mongo db connected------error",error);
-    
+db.on('error', (error) => {
+    console.log("mongo db connected------error", error);
+
 })
 
-db.on('disconnected',()=>{
+db.on('disconnected', () => {
     console.log("mongo db dis----connected------");
-    
+
 })
 module.exports = db;
